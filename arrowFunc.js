3fe262f4e7573f6d0db9a89c485988c06e3fff6e
reeba-hasan <reebahasan.syeda@gmail.com>
2024-09-20 15:15:00 +0500

    feat: arrowFunct.js code

---
+++ arrowFunc.js
@@ -0,0 +1,15 @@
+/*const x = {
+    name: "ree",
+    role: "frontend dev",
+    exp: 3,
+    show() {
+        console.log(`The name is ${this.name}`)
+    }
+}
+x.show()*/
+
+const sayHello = (name , hemlo)=> {
+    console.log("greetings" + " " + name + " " + hemlo)
+    console.log("hi")
+}
+sayHello("reeba", "good afternoon")
---
+++ ifElse.js
@@ -0,0 +1,21 @@
+/*let hour = 13;
+
+if (hour >= 6 && hour < 12) 
+console.log('Good morning');
+
+else if (hour >= 12 && hour < 18) 
+console.log('Good Afternoon!');
+
+else 
+console.log('Good Evening!'); */
+
+
+/*for (let i = 1; i <= 5; i++) {
+    if(i % 2 !== 0) console.log(i);
+}*/
+
+for (let i = 5; i >= 1; i--) {
+    if(i % 2 !== 0) console.log(i);
+}
+
+
